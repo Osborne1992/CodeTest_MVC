38aa7ca377af9568e45de772de0427c0062d8d4b
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2017-03-27 12:17:17 +0100

    corrected typo in vehicle controller for registration field

--- MVC_CodeTest/app/controllers/vehicles_controller.rb
+++ MVC_CodeTest/app/controllers/vehicles_controller.rb
@@ -69,6 +69,6 @@ class VehiclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_params
-      params.require(:vehicle).permit(:id, :price, :registraton, :manufacturer)
+      params.require(:vehicle).permit(:id, :price, :registration, :manufacturer)
     end
 end
